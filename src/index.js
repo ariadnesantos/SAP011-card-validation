f5d27079a51da38301dee2d48d49f33f52e963c7
ariadnesantos <ariadne.santos1910@gmail.com>
2023-08-03 14:15:11 -0300

    Atualização 0308

--- src/index.js
+++ src/index.js
@@ -1,84 +1,78 @@
 import validator from './validator.js';
-console.log(validator);
-// ESCUTAR OS EVENTOS DE DOM, chamar validator.isValid() e validator.maskify()
 
-  const form = document.getElementById("form");
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const expiryDate = document.getElementById("expiryDate");
-  const cvc = document.getElementById("cvc");
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const expiryDate = document.getElementById("expiryDate");
+const cvc = document.getElementById("cvc");
+const number = document.getElementById("creditCardNumber");
+const result = document.getElementById("result");
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    checkInputs();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkInputs();
 
-    //criar const e chamar o number do cartão
-    validator.validateCreditCard(number);
-  });
+  if (validator.isValid(number.value)) {
+    result.innerHTML = "Cartão válido";
+  } else {
+    result.innerHTML = "Cartão inválido";
+  }
+});
 
-  function checkInputs() {
-    const nameValue = name.value;
-    const emailValue = email.value;
-    const expiryDateValue = expiryDate.value;
-    const cvcValue = cvc.value;
+function checkInputs() {
+  const nameValue = name.value;
+  const emailValue = email.value;
+  const expiryDateValue = expiryDate.value;
+  const cvcValue = cvc.value;
 
-    if (nameValue === "") {
-      setErrorFor(name, "O nome de usuário é obrigatório.");
-    } else {
-      setSuccessFor(name);
-    }
-
-    if (emailValue === "") {
-      setErrorFor(email, "O e-mail é obrigatório.");
-    } else if (!checkEmail(emailValue)) {
-      setErrorFor(email, "Por favor, insira um e-mail válido.");
-    } else {
-      setSuccessFor(email);
-    }
-
-    if (expiryDateValue === "") {
-      setErrorFor(expiryDate, "A data de validade do cartão é obrigatória.");
-    } else {
-      setSuccessFor(expiryDate);
-    }
-
-    if (cvcValue === "") {
-      setErrorFor(cvc, "O código de verificação do cartão é obrigatório.");
-    } else {
-      setSuccessFor(cvc);
-    }
+  if (nameValue.trim() === "") {
+    setErrorFor(name, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(name);
   }
 
-    const formControls = form.querySelectorAll(".form-control");
-
-    const formIsValid = [...formControls].every((formControl) => {
-      return formControl.classList.contains("success");
-    });
-
-    if (formIsValid) {
-      alert ("O formulário está 100% válido!");
-      // Adicionar aqui o alert para dizer se o formulário está 100% válido  
-    }
-
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-
-   // Esse small não está funcionando
-    small.innerText = message;
-    formControl.classList.remove("success");
-    formControl.classList.add("error");
-  
-
-  function setSuccessFor(input) {
-    const formControl = input.parentElement;
-
-    formControl.classList.remove("error");
-    formControl.classList.add("success");
+  if (emailValue.trim() === "") {
+    setErrorFor(email, "O e-mail é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um e-mail válido.");
+  } else {
+    setSuccessFor(email);
   }
 
-  function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  if (expiryDateValue.trim() === "") {
+    setErrorFor(expiryDate, "A data de validade do cartão é obrigatória.");
+  } else {
+    setSuccessFor(expiryDate);
   }
 
-    
+  if (cvcValue.trim() === "") {
+    setErrorFor(cvc, "O código de verificação do cartão é obrigatório.");
+  } else {
+    setSuccessFor(cvc);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  formControl.classList.remove("success");
+  formControl.classList.add("error");
+  small.innerText = message;
+  small.style.visibility = "visible";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  formControl.classList.remove("error");
+  formControl.classList.add("success");
+  small.style.visibility = "hidden";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
 
--- src/validator.js
+++ src/validator.js
@@ -1,41 +1,35 @@
+const validator = {
+  isValid: function (creditCardNumber) {
+    const cleanedNumber = creditCardNumber.replace(/\s/g, '');
+    const digitsArray = Array.from(cleanedNumber).map(Number);
 
-  //validator.isValid(creditCardNumber): creditCardNumber é um string com o número do cartão a ser verificado. Esta função deve retornar um boolean, dependendo de sua validade, de acordo com o algoritmo de Luhn.
-  //validator.maskify(creditCardNumber): creditCardNumber é um string com o número do cartão e esta função deve retornar um string onde todos, exceto os últimos quatro caracteres, são substituídos por um número (#) ou 🐱. 
-  //Essa função deve sempre manter os quatro últimos caracteres intactos, mesmo quando o string for mais curto.
-  //Exemplo de uso:
-  //maskify('4556364607935616') === '############5616'
-  //maskify(     '64607935616') ===      '#######5616'
-  //maskify(               '1') ===                '1'
-  //maskify(                '') ===                ''
-  
-  //1° Verificar se o cartão é válido
+    digitsArray.reverse();
 
-  // Passo 1 - inverter o número do cartão
-  function validateCreditCard(number) {
-    const invertedCard = number.split("").reverse().join("");
-  // Passo 2 - Transformar os caracteres em números e criar um array
-    const digits = invertedCard.split("").map(Number);
-  // Passo 3 - Dobrar os dígitos em posições pares (índices ímpares)
-    for (let i = 1; i < digits.length; i += 2) {
-      digits[i] *= 2;
-  // Se o dígito dobrado for maior que 9, preciso subtrair 9
-    if (digits [i] > 9) {
-      digits[i] -=9;
+    let sum = 0;
+    for (let i = 0; i < digitsArray.length; i++) {
+      if (i % 2 !== 0) {
+        let doubleDigits = digitsArray[i] * 2;
+        if (doubleDigits > 9) {
+          doubleDigits -= 9;
+        }
+        sum += doubleDigits;
+      } else {
+        sum += digitsArray[i];
+      }
     }
+
+    // Verificar se o resultado da soma é um múltiplo de 10
+    return sum % 10 === 0;
+  },
+  maskify: function (creditCardNumber) {
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber;
     }
-  // Passo 4 - Somar todos os dígitos
-    const sumDigits = digits.reduce((total, digit) => total + digit, 0);
-  // Passo 5 - Verificar se a soma é múltipla de 10
-    return sumDigits % 10 === 0;
-    }
-  // Passo 6 - Adicionar o evento de escuta ao botão 'verification' (eu achooo) ou coloco no index?
-
-
-
-//Fiquei confusa sobre quais informações deixar aqui e quais passar para o index.js
-//Onde exatamente vai o const validator?
-    const validator = {
- validateCreditCard, //import e export
-  };
+    const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+    return maskedString;
+  },
+};
 
 export default validator;
+
+ 
