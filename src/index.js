2c7f111a8862419228bf80b43d5d8f0e2495545d
ariadnesantos <ariadne.santos1910@gmail.com>
2023-08-04 12:43:26 -0300

    Teste Validator

--- src/index.js
+++ src/index.js
@@ -1,78 +1,7 @@
-import validator from './validator.js';
-
-const form = document.getElementById("form");
-const name = document.getElementById("name");
+//// teste sem o index.js para ver se o validator funciona////
 const email = document.getElementById("email");
-const expiryDate = document.getElementById("expiryDate");
-const cvc = document.getElementById("cvc");
-const number = document.getElementById("creditCardNumber");
-const result = document.getElementById("result");
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  checkInputs();
 
-  if (validator.isValid(number.value)) {
-    result.innerHTML = "Cartão válido";
-  } else {
-    result.innerHTML = "Cartão inválido";
-  }
-});
 
-function checkInputs() {
-  const nameValue = name.value;
-  const emailValue = email.value;
-  const expiryDateValue = expiryDate.value;
-  const cvcValue = cvc.value;
-
-  if (nameValue.trim() === "") {
-    setErrorFor(name, "O nome de usuário é obrigatório.");
-  } else {
-    setSuccessFor(name);
-  }
-
-  if (emailValue.trim() === "") {
-    setErrorFor(email, "O e-mail é obrigatório.");
-  } else if (!checkEmail(emailValue)) {
-    setErrorFor(email, "Por favor, insira um e-mail válido.");
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (expiryDateValue.trim() === "") {
-    setErrorFor(expiryDate, "A data de validade do cartão é obrigatória.");
-  } else {
-    setSuccessFor(expiryDate);
-  }
-
-  if (cvcValue.trim() === "") {
-    setErrorFor(cvc, "O código de verificação do cartão é obrigatório.");
-  } else {
-    setSuccessFor(cvc);
-  }
-}
-
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  formControl.classList.remove("success");
-  formControl.classList.add("error");
-  small.innerText = message;
-  small.style.visibility = "visible";
-}
-
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  formControl.classList.remove("error");
-  formControl.classList.add("success");
-  small.style.visibility = "hidden";
-}
-
-function checkEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,35 +1,50 @@
-const validator = {
-  isValid: function (creditCardNumber) {
-    const cleanedNumber = creditCardNumber.replace(/\s/g, '');
-    const digitsArray = Array.from(cleanedNumber).map(Number);
 
-    digitsArray.reverse();
-
-    let sum = 0;
-    for (let i = 0; i < digitsArray.length; i++) {
-      if (i % 2 !== 0) {
-        let doubleDigits = digitsArray[i] * 2;
-        if (doubleDigits > 9) {
-          doubleDigits -= 9;
-        }
-        sum += doubleDigits;
+  document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('creditCardForm');
+    const resultDiv = document.getElementById('result');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+  
+      const cardNumber = document.getElementById('cardNumber').value;
+  
+      if (isValid(cardNumber)) {
+        resultDiv.textContent = 'Cartão Válido';
       } else {
-        sum += digitsArray[i];
+        resultDiv.textContent = 'Cartão Inválido';
       }
+  
+      maskifyCardNumber(cardNumber);
+    });
+  
+    function isValid(cardNumber) {
+      const cleanedNumber = cardNumber.replace(/\s/g, '');
+      const digitsArray = Array.from(cleanedNumber).map(Number);
+  
+      digitsArray.reverse();
+  
+      let sum = 0;
+      for (let i = 0; i < digitsArray.length; i++) {
+        if (i % 2 !== 0) {
+          let doubleDigits = digitsArray[i] * 2;
+          if (doubleDigits > 9) {
+            doubleDigits -= 9;
+          }
+          sum += doubleDigits;
+        } else {
+          sum += digitsArray[i];
+        }
+      }
+  
+      return sum % 10 === 0;
     }
-
-    // Verificar se o resultado da soma é um múltiplo de 10
-    return sum % 10 === 0;
-  },
-  maskify: function (creditCardNumber) {
-    if (creditCardNumber.length <= 4) {
-      return creditCardNumber;
+  
+    function maskifyCardNumber(cardNumber) {
+      const masked = cardNumber.slice(0, -4).replace(/\d/g, '#') + cardNumber.slice(-4);
+      document.getElementById('cardNumber').value = masked;
     }
-    const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
-    return maskedString;
-  },
-};
+  });
 
-export default validator;
 
+  
  
