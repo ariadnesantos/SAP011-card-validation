a49143caf1c1b09c9415c96910b4fd710af035ff
ariadnesantos <ariadne.santos1910@gmail.com>
2023-08-05 17:11:46 -0300

    Atualização 05/08 funcionando

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 //// teste sem o index.js para ver se o validator funciona////
 
-import validator from './validator';
-
-console.log(validator);
+import validator from './validator.js';
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,56 +1,56 @@
-
-
-
-  isValid: document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("form");
-    const resultDiv = document.getElementById("result");
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const cardNumber = document.getElementById('cardNumber').value;
-  
-      if (isValid(cardNumber)) {
-        resultDiv.textContent = 'Cartão Válido';
-      } else {
-        resultDiv.textContent = 'Cartão Inválido';
-      }
-  
-      maskifyCardNumber(cardNumber);
-    });
-  
-    function isValid(cardNumber) {
-      const cleanedNumber = cardNumber.replace(/\s/g, '');
-      const digitsArray = Array.from(cleanedNumber).map(Number);
-  
-      digitsArray.reverse();
-  
-      let sum = 0;
-      for (let i = 0; i < digitsArray.length; i++) {
-        if (i % 2 !== 0) {
-          let doubleDigits = digitsArray[i] * 2;
-          if (doubleDigits > 9) {
-            doubleDigits -= 9;
+       
+       document.addEventListener("DOMContentLoaded", function () {
+        const form = document.getElementById("form");
+        const resultDiv = document.getElementById("result");
+      
+        form.addEventListener("submit", function (event) {
+          event.preventDefault();
+      
+          const cardNumber = document.getElementById("cardNumber").value;
+      
+          if (isValid(cardNumber)) {
+            resultDiv.textContent = "Cartão Válido!";
+          } else {
+            resultDiv.textContent = "Por favor, insira um cartão de crédito válido.";
           }
-          sum += doubleDigits;
-        } else {
-          sum += digitsArray[i];
+      
+          maskifyCardNumber(cardNumber);
+        });
+    });
+    
+        function isValid(cardNumber) {
+          const cleanedNumber = cardNumber.replace(/\s/g, '');
+          const digitsArray = Array.from(cleanedNumber).map(Number);
+      
+          digitsArray.reverse();
+      
+          let sum = 0;
+          for (let i = 0; i < digitsArray.length; i++) {
+            if (i % 2 !== 0) {
+              let doubleDigits = digitsArray[i] * 2;
+              if (doubleDigits > 9) {
+                doubleDigits -= 9;
+              }
+              sum += doubleDigits;
+            } else {
+              sum += digitsArray[i];
+            }
+          }
+      
+          return sum % 10 === 0;
         }
-      }
-  
-      return sum % 10 === 0;
-    }
-  
-    function maskifyCardNumber(cardNumber) {
-      const masked = cardNumber.slice(0, -4).replace(/\d/g, '#') + cardNumber.slice(-4);
-      document.getElementById('cardNumber').value = masked;
-    }
-  });
+      
+        function maskifyCardNumber(cardNumber) {
+          const masked = cardNumber.slice(0, -4).replace(/\d/g, '#') + cardNumber.slice(-4);
+          document.getElementById("cardNumber").value = masked;
+        }
 
-  const validator = {
-      isValid, maskifyCardNumber
-   } 
+        
+    
+        const validator = {
+          isValid, maskifyCardNumber
+        }
 
-   export default validator;
-  
- 
+      
+
+export default validator.js;
